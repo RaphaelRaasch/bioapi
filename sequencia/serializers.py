3ac0068026d96595c9e36fe868872f038191e332
RaphaelRaasch <raaschfialho@gmail.com>
2020-08-14 13:00:08 -0300

    cliente api

--- sequencia/serializers.py
+++ sequencia/serializers.py
@@ -1,10 +1,13 @@
 
 from rest_framework.serializers import ModelSerializer
-from .models import Sequencia 
+from .models import Sequencia
+from clientes.serializers import ClienteSerializer
 
 
 class SequenciaSerializers(ModelSerializer):
-    
+
+    cliente = ClienteSerializer(many=False)
+
     class Meta:
         model = Sequencia
         fields = ('numero', 'mtr', 'cliente', 'rua', 'numero', 'bairro', 'municipio', 'estado', 'lat', 'long')
